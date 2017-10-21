f44d2255a26ce286272650aa9ed1a717100549c9
bizzaro <albert.fung@uoit.net>
2017-10-21 07:50:10 -0400

    changed kill to pkill and added option to kill parent processes

--- watch.sh
+++ watch.sh
@@ -8,16 +8,15 @@ echo "STARTED PID $pid"
 
 while true; do
     #use trap to catch CTRL-C (SIGINT)
-    trap "kill -9 $pid; exit" INT
+    trap "pkill -TERM -P $pid; exit" INT
     now=`stat -f %z $1`
     date=`date`
     if [[ $now != $ori ]]; then
         echo "--------"
         echo "Change detected @ $date"
         #send kill command to old run time
-        kill -9 $pid
+        pkill -TERM -P $pid
         #hide ugly kill message
-        wait $pid 2> /dev/null
         $2 &
         pid=$!
         echo "STARTED PID $pid"
